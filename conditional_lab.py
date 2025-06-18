4a5e628194b02a069a0d8587f1e1137d61282e8b
shahad-bf <shoshomohd393@gmail.com>
2025-06-18 11:30:21 +0300

    condtional_lab

---
+++ conditional_lab.py
@@ -0,0 +1,22 @@
+name_movie= "DALIDA"
+rate_movie=3
+popularity_score=72.65
+
+if rate_movie >= 4 and popularity_score > 80:
+    print("Highly recommended")
+
+elif rate_movie >= 3 and popularity_score > 70: 
+    print("I recommended it, is good")  
+
+elif rating <= 2 and popularity > 60:
+    print("You should check it out!")
+elif rating <= 2 and popularity < 50:
+    print("Don't watch it. It is a waste of time")
+else:
+    print("Not enough data to give a recommendation.")    
+
+
+
+
+
+
